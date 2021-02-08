28fda26d9aee765e6acb718ebeebe968a84d5880
Gina Beth Russell <ginabethrussell@Ginas-MBP.cinci.rr.com>
2021-02-08 15:04:55 -0500

    created Snackbar app file structure and Main.java file

---
+++ src/Snackbar/Main.java
@@ -0,0 +1,14 @@
+package Snackbar;
+
+public class Main 
+{
+	private static void operateSnackbar()
+	{
+		System.out.println("Snackbar is open!");
+	}
+
+	public static void main(String[] args)
+	{
+		operateSnackbar();
+	}
+}
